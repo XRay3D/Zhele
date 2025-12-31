b2f9eb4239f1deb8b2262fdd19775296670202e7
Бакиев Дамир <bakiev@elemer.ru>
2025-12-31 17:05:50 +0300

    Added support for bitwise operations between enumerations and integers.

--- include/zhele/common/macro_utils/enum.h
+++ include/zhele/common/macro_utils/enum.h
@@ -16,6 +16,7 @@
 #include <utility>
 
 template <typename E> concept Enum = std::is_enum_v<E>;
+template <typename E> concept EnumOrIntegral = std::is_enum_v<E> || std::is_integral_v<E>;
 
 #if __cpp_lib_to_underlying >= 202102L
 using std::to_underlying;
@@ -37,24 +38,27 @@ template <Enum E>
 [[nodiscard]] constexpr E operator--(E& e, int) noexcept { return std::exchange(e, static_cast<E>(+e - 1)); }
 
 #define DECLARE_VIEWS_IOTA(ENUM)                                        \
-template <> struct std::incrementable_traits<ENUM> {                \
-      using difference_type = make_signed_t<underlying_type_t<ENUM>>; \
-};
+    template <> struct std::incrementable_traits<ENUM> {                \
+        using difference_type = make_signed_t<underlying_type_t<ENUM>>; \
+    };
 
-template <Enum E>
-[[nodiscard]] constexpr E operator&(E left, E right) noexcept { return static_cast<E>(+left & +right); }
-template <Enum E>
-[[nodiscard]] constexpr E operator^(E left, E right) noexcept { return static_cast<E>(+left ^ +right); }
-template <Enum E>
-[[nodiscard]] constexpr E operator|(E left, E right) noexcept { return static_cast<E>(+left | +right); }
+template <EnumOrIntegral L, EnumOrIntegral R, typename Ret = std::conditional_t<Enum<L>, L, R>>
+    requires Enum<L> || Enum<R>
+[[nodiscard]] constexpr Ret operator&(L left, R right) noexcept { return static_cast<Ret>(+left & +right); }
+template <EnumOrIntegral L, EnumOrIntegral R, typename Ret = std::conditional_t<Enum<L>, L, R>>
+    requires Enum<L> || Enum<R>
+[[nodiscard]] constexpr Ret operator^(L left, R right) noexcept { return static_cast<Ret>(+left ^ +right); }
+template <EnumOrIntegral L, EnumOrIntegral R, typename Ret = std::conditional_t<Enum<L>, L, R>>
+    requires Enum<L> || Enum<R>
+[[nodiscard]] constexpr Ret operator|(L left, R right) noexcept { return static_cast<Ret>(+left | +right); }
 template <Enum E>
 [[nodiscard]] constexpr E operator~(E left) noexcept { return static_cast<E>(~+left); }
-template <Enum E>
-[[nodiscard]] constexpr const E& operator&=(E& left, E right) noexcept { return left = left & right; }
-template <Enum E>
-[[nodiscard]] constexpr const E& operator^=(E& left, E right) noexcept { return left = left ^ right; }
-template <Enum E>
-[[nodiscard]] constexpr const E& operator|=(E& left, E right) noexcept { return left = left | right; }
+template <Enum L, EnumOrIntegral R, typename Ret = std::conditional_t<Enum<L>, L, R>>
+[[nodiscard]] constexpr const L& operator&=(L& left, R right) noexcept { return left = left & right; }
+template <Enum L, EnumOrIntegral R, typename Ret = std::conditional_t<Enum<L>, L, R>>
+[[nodiscard]] constexpr const L& operator^=(L& left, R right) noexcept { return left = left ^ right; }
+template <Enum L, EnumOrIntegral R, typename Ret = std::conditional_t<Enum<L>, L, R>>
+[[nodiscard]] constexpr const L& operator|=(L& left, R right) noexcept { return left = left | right; }
 
 namespace Zhele {
 template <Enum Enum>
@@ -68,18 +72,31 @@ constexpr bool HasAnyFlag(Enum value, Enum flags) noexcept { return (+value & +f
 namespace Test {
 
 enum class E : signed char {
-  A,
-  B,
-  C,
-  D
+    A,
+    B,
+    C,
+    D
 };
 
 static_assert((E::B & E::C) == E::A);
 static_assert((E::B ^ E::C) == E::D);
 static_assert((E::B | E::C) == E::D);
+
+static_assert((E::B & 2) == E::A);
+static_assert((E::B ^ 2) == E::D);
+static_assert((E::B | 2) == E::D);
+
+static_assert((1 & E::C) == E::A);
+static_assert((1 ^ E::C) == E::D);
+static_assert((1 | E::C) == E::D);
+
 static_assert(~E::A == static_cast<E>(-1));
 static_assert(Zhele::HasAllFlags(E::D, E::B | E::C));
 static_assert(Zhele::HasAnyFlag(E::D, E::C));
+
+static_assert([](E e) consteval { return e &= 2; }(E::B) == E::A);
+static_assert([](E e) consteval { return e ^= 2; }(E::B) == E::D);
+static_assert([](E e) consteval { return e |= 2; }(E::B) == E::D);
 
 static_assert([](E e) consteval { return e++; }(E::B) == E::B);
 static_assert([](E e) consteval { return e--; }(E::B) == E::B);
