6207360fd50c23830b590d5a7229988a339d4625
XRay3D <xray3d@ya.ru>
2026-01-02 13:42:36 +0300

    Fix include paths and add missing header
    Corrected the include path for data_transfer.h and added enum.h.

--- include/zhele/common/usart.h
+++ include/zhele/common/usart.h
@@ -10,7 +10,8 @@
 #ifndef ZHELE_UART_COMMON_H
 #define ZHELE_UART_COMMON_H
 
-#include "./template_utils/data_transfer.h"
+#include "template_utils/data_transfer.h"
+#include "macro_utils/enum.h"
 #include "ioreg.h"
 
 #include <zhele/clock.h>
